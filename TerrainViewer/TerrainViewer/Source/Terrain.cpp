f5fa347a717c51b3b6ecb21fcf0b18453f9e22d3
cislunar <jackbehjohn@gmail.com>
2013-11-10 12:06:44 -0800

    Terrain Base Rendering

---
+++ TerrainViewer/TerrainViewer/Source/Terrain.cpp
@@ -0,0 +1,200 @@
+#include "Terrain.h"
+#include "Simulation.h"
+
+void Terrain::CreateVBO()
+{
+
+	glUseProgram(m_renderInfo.ProgramId);
+
+	// Vertex array object (creates a place to store vertices)
+	glGenVertexArrays(1, &m_renderInfo.VaoId);
+	glBindVertexArray(m_renderInfo.VaoId);
+	printOpenGLError();
+
+	// Enables vertex shader attributes for use
+	glEnableVertexAttribArray(0);
+
+	// creates a buffer object to transfer vertices data to vertex array
+	// then buffers the data
+	glGenBuffers(1, &m_renderInfo.VboId);
+	glBindBuffer(GL_ARRAY_BUFFER, m_renderInfo.VboId);
+	glBufferData(	GL_ARRAY_BUFFER, 
+					sizeof(glm::vec4) * m_vertResolution.x * m_vertResolution.y, 
+					m_vertices, 
+					GL_STATIC_DRAW);
+	printOpenGLError();
+
+	// defines the data we just transferred to the gpu
+	glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 0, (GLvoid*)0);
+	printOpenGLError();
+
+	// Determines the draw order of the vertices we transferred to gpu
+	glGenBuffers(1, &m_renderInfo.IndexBufferId);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_renderInfo.IndexBufferId);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLuint) * IndicesCount(), m_indices, GL_STATIC_DRAW);
+	printOpenGLError();
+
+	// Detaches vertex array
+	glBindVertexArray(0);
+	glUseProgram(0);
+}
+
+void Terrain::CreateShaders()
+{
+	// Load the shader
+	ShaderFiles sFiles;
+	sFiles.fragFile = (char *)malloc(sizeof(char) * (32));
+	sFiles.vertFile = (char *)malloc(sizeof(char) * (32));
+	memcpy(sFiles.vertFile, "Source\\TerrainVert.txt", sizeof(char) * (32));
+	memcpy(sFiles.fragFile, "Source\\TerrainFrag.txt", sizeof(char) * (32));
+	LoadShader(&m_renderInfo, sFiles);
+
+	ModelMatrixUniformLocation = glGetUniformLocation(m_renderInfo.ProgramId, "ModelMatrix");
+	ViewMatrixUniformLocation = glGetUniformLocation(m_renderInfo.ProgramId, "ViewMatrix");
+	ProjectionMatrixUniformLocation = glGetUniformLocation(m_renderInfo.ProgramId, "ProjectionMatrix");
+	printOpenGLError();
+
+	free(sFiles.fragFile);
+	free(sFiles.vertFile);
+}
+
+void Terrain::DestroyShaders()
+{
+	glDetachShader(m_renderInfo.ProgramId, m_renderInfo.FragmentShaderId);
+	glDetachShader(m_renderInfo.ProgramId, m_renderInfo.VertexShaderId);
+	printOpenGLError();
+
+	glDeleteShader(m_renderInfo.FragmentShaderId);
+	glDeleteShader(m_renderInfo.VertexShaderId);
+	printOpenGLError();
+
+	glDeleteProgram(m_renderInfo.ProgramId);
+	printOpenGLError();
+}
+
+void Terrain::DestroyVBO()
+{
+	UnbindForRender();
+	glDeleteBuffers(1, &m_renderInfo.IndexBufferId);
+	glDeleteBuffers(1, &m_renderInfo.VboId);
+	glDeleteVertexArrays(1, &m_renderInfo.VaoId);
+	printOpenGLError();
+}
+
+int Terrain::TriCount()
+{
+	return m_faceResolution.x * m_faceResolution.y * 2;
+}
+
+int Terrain::IndicesCount()
+{
+	return TriCount() * 3;
+}
+
+
+void Terrain::Render()
+{
+
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LESS);
+
+	glEnable(GL_CULL_FACE);
+	glCullFace(GL_BACK);
+	glFrontFace(GL_CW);
+
+	BindForRender();
+	glDrawElements(GL_TRIANGLES, IndicesCount(), GL_UNSIGNED_INT, (GLvoid*)0);
+	UnbindForRender();
+
+	glDisable(GL_CULL_FACE);
+	glDisable(GL_DEPTH_TEST);
+	printOpenGLError();
+}
+
+glm::mat4 Terrain::GetModelMat()
+{
+	// Identity matrix times scale of this object
+	glm::mat4 retval = glm::mat4(1.0f);
+	return 	glm::scale(retval, glm::vec3(m_scale,m_scale,m_scale) );
+}
+
+
+void Terrain::BindForRender()
+{
+	// use the shader
+	glUseProgram(m_renderInfo.ProgramId);
+	printOpenGLError();
+
+
+	Simulation *sim = Simulation::GetSimulation();
+
+	// Buffer the matrices
+	glUniformMatrix4fv(ModelMatrixUniformLocation, 1, GL_FALSE, &(GetModelMat())[0][0] );
+	// THIS NEEDS TO BE VIEW MATRIX
+	glUniformMatrix4fv(ViewMatrixUniformLocation, 1, GL_FALSE, &(sim->GetViewMat())[0][0]);
+	// Then get the perspective matrix
+	glUniformMatrix4fv(ProjectionMatrixUniformLocation, 1, GL_FALSE, &(sim->GetProjMat())[0][0]);
+
+	// bind the vertex array
+	glBindVertexArray(m_renderInfo.VaoId);
+}
+
+void Terrain::UnbindForRender()
+{
+	glBindVertexArray(0);
+	glUseProgram(0);
+}
+
+void Terrain::Init()
+{
+	// Here, we evenly distribute the vertices of the terrain
+	// based on the face counts
+	float xPos = -.5f;
+	float zPos = .5f;
+	float xStart = -.5f;
+	float wInc = 1.f/m_faceResolution.x;
+	float dInc = 1.f/m_faceResolution.y;
+	int idx = 0;
+	for(uint16_t i=0; i < m_vertResolution.y; ++i)
+	{
+		for(uint16_t j=0; j < m_vertResolution.x; ++j)
+		{
+			idx = i * m_vertResolution.x + j;
+			m_vertices[idx] = glm::vec4(xPos, 0.f, zPos, 1.f);
+			xPos+=wInc;
+		}
+		xPos = xStart;
+		zPos-=dInc;
+	}
+
+	int v1 = 0;
+	int v2 = 0;
+	int v3 = 0;
+	int v4 = 0;
+	int idxCnt = 0;
+
+	for(uint16_t i=0; i<m_faceResolution.y; ++i)
+	{
+		for(uint16_t j=0; j<m_faceResolution.x; ++j)
+		{
+			// We use the face Idx to determine the vertex Idx we want
+			// with the following calculation. It works like an ordinary
+			// 2D array indexer, but with the added bonus of ignoring
+			// vertices that reside on the side of the plane
+			// and thus cannot be the starting corner of a face
+			v1 = i * m_faceResolution.x + j + i;	// upper left vertex
+			v2 = v1 + m_vertResolution.x;			// bot left vertex
+			v3 = v2 + 1;							// bot right vertex
+			v4 = v1 + 1;							// upper right vertex
+
+			// We now add the indices that represent a triangle in CCW fashion,
+			// 2 triangles per face
+			m_indices[idxCnt++] = v1;
+			m_indices[idxCnt++] = v2;
+			m_indices[idxCnt++] = v3;
+			m_indices[idxCnt++] = v3;
+			m_indices[idxCnt++] = v4;
+			m_indices[idxCnt++] = v1;
+		}
+	}
+}
---
+++ TerrainViewer/TerrainViewer/Source/Terrain.h
@@ -0,0 +1,69 @@
+#pragma once
+#include "RenderObjBase.h"
+#include <stdint.h>
+
+class Terrain : public RenderObjBase
+{
+public:
+	Terrain() : RenderObjBase()
+	{
+		m_position = glm::vec4(0,0,0,1);
+		m_scale = 100.0f;
+		m_faceResolution = glm::uvec2(1024,1024);
+		m_vertResolution = m_faceResolution + glm::uvec2(1,1);
+
+		m_vertices = (glm::vec4*)malloc(sizeof(glm::vec4) * m_vertResolution.y * m_vertResolution.x);
+		if(m_vertices == NULL)
+		{
+			fprintf(stderr, "out of memory\n");
+		}
+		memset(m_vertices, 0, sizeof(glm::vec4) * m_vertResolution.y * m_vertResolution.x);
+
+		m_indices = (GLuint*)malloc(sizeof(GLuint) * IndicesCount());
+		if(m_indices == NULL)
+		{
+			fprintf(stderr, "out of memory\n");
+		}
+		Init();
+	}
+
+	~Terrain()
+	{
+		if(m_indices != NULL)
+		{
+			free(m_indices);
+			m_indices = NULL;
+		}
+
+		if(m_vertices != NULL)
+		{
+			free(m_vertices);
+			m_vertices = NULL;
+		}
+	}
+	virtual void Render(void);
+	virtual glm::mat4 GetModelMat();
+
+protected:
+	virtual void CreateVBO(void);
+	virtual void CreateShaders(void);
+	virtual void DestroyVBO(void);
+	virtual void DestroyShaders(void);
+	void BindForRender();
+	void UnbindForRender();
+	int TriCount();
+	int IndicesCount();
+private:
+	void Init();
+
+	glm::vec4* m_vertices;
+	GLuint* m_indices; 
+	glm::uvec2 m_faceResolution;
+	glm::uvec2 m_vertResolution;
+	glm::vec4 m_position;
+	float m_scale;
+	GLuint
+		ProjectionMatrixUniformLocation,
+		ViewMatrixUniformLocation,
+		ModelMatrixUniformLocation;
+};
