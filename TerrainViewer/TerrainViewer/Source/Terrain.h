ccf8d406d3f1603701cf91e7febf85ab3c8fe7c6
cislunar <jackbehjohn@gmail.com>
2013-11-16 19:20:03 -0800

    Fixed Terrain Rendering
    Added back in texture functions

--- TerrainViewer/TerrainViewer/Source/Terrain.h
+++ TerrainViewer/TerrainViewer/Source/Terrain.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "RenderObjBase.h"
 #include <stdint.h>
+#include "TextureData.h"
 
 class Terrain : public RenderObjBase
 {
@@ -13,12 +14,12 @@ public:
 		m_faceResolution = glm::uvec2(256,256);
 		m_vertResolution = m_faceResolution + glm::uvec2(1,1);
 
-		m_vertices = (glm::vec4*)malloc(sizeof(glm::vec4) * m_vertResolution.y * m_vertResolution.x);
+		m_vertices = (glm::vec4*)malloc(sizeof(glm::vec4) * VerticesCnt() );
 		if(m_vertices == NULL)
 		{
 			fprintf(stderr, "out of memory\n");
 		}
-		memset(m_vertices, 0, sizeof(glm::vec4) * m_vertResolution.y * m_vertResolution.x);
+		memset(m_vertices, 0, sizeof(glm::vec4) * VerticesCnt() );
 
 		m_indices = (GLuint*)malloc(sizeof(GLuint) * IndicesCount());
 		if(m_indices == NULL)
@@ -27,21 +28,27 @@ public:
 		}
 		memset(m_indices, 0, sizeof(GLuint) * IndicesCount());
 
-		m_texCoords = (glm::vec2*)malloc(sizeof(glm::vec2) * m_vertResolution.y * m_vertResolution.x);
+		m_texCoords = (glm::vec2*)malloc(sizeof(glm::vec2) * VerticesCnt() );
 		if(m_texCoords == NULL)
 		{
 			fprintf(stderr, "out of memory\n");
 		}
-		memset(m_texCoords, 0, sizeof(glm::vec2) * m_vertResolution.y * m_vertResolution.x);
+		memset(m_texCoords, 0, sizeof(glm::vec2) * VerticesCnt() );
 
-		m_vertNormals = (glm::vec3*)malloc(sizeof(glm::vec3) * m_vertResolution.y * m_vertResolution.x);
+		m_vertNormals = (glm::vec3*)malloc(sizeof(glm::vec3) * VerticesCnt() );
 		if(m_vertNormals == NULL)
 		{
 			fprintf(stderr, "out of memory\n");
 		}
-		memset(m_vertNormals, 0, sizeof(glm::vec3) * m_vertResolution.y * m_vertResolution.x);
+		memset(m_vertNormals, 0, sizeof(glm::vec3) * VerticesCnt() );
 
-		Init();
+		m_heightData = (float*)malloc(sizeof(float) * VerticesCnt() );
+		if(m_heightData == NULL)
+		{
+			fprintf(stderr, "out of memory\n");
+		}
+		memset(m_heightData, 0, sizeof(float) * VerticesCnt() );
+
 	}
 
 	~Terrain()
@@ -69,6 +76,12 @@ public:
 			free(m_vertNormals);
 			m_vertNormals = NULL;
 		}
+
+		if(m_heightData != NULL)
+		{
+			free(m_heightData);
+			m_heightData = NULL;
+		}
 	}
 	virtual void Render(void);
 	virtual glm::mat4 GetModelMat();
@@ -84,14 +97,20 @@ protected:
 	void BindForRender();
 	void UnbindForRender();
 	int TriCount();
+	int VerticesCnt();
 	int IndicesCount();
+	glm::vec4 GetTexel( TextureData* _texData,  int _x, int _y);
+	void GetTextureData( GLuint _tex, GLenum _texType, TextureData* _texData );
+	glm::vec4 SampleTexture_Linear( glm::vec2 _coords, TextureData* _texData );
+	void GetHeightData();
+	void Terrain::SmoothVertices();
 private:
-	void Init();
 	void InitTriVertices();
 	void InitTriIndices();
 
 	glm::vec4* m_vertices;
 	GLuint* m_indices; 
+	float* m_heightData;
 	glm::vec2* m_texCoords;
 	glm::vec3* m_vertNormals;
 	glm::uvec2 m_faceResolution;
---
+++ TerrainViewer/TerrainViewer/Source/TextureData.h
@@ -0,0 +1,42 @@
+#pragma once
+#include "Render.h"
+#include "Math.h"
+#include <stdint.h>
+
+class TextureData
+{
+public:
+	TextureData()
+	{
+		m_texData = NULL;
+	}
+	~TextureData()
+	{
+		if(m_texData != NULL)
+		{
+			free(m_texData);
+			m_texData = NULL;
+		}
+	}
+
+	void SetupData( size_t _bytes)
+	{
+		if(m_texData != NULL)
+		{
+			free(m_texData);
+		}
+		m_texData = (unsigned char*)malloc( _bytes );
+	}
+
+	int TexByteWidth()
+	{
+		return m_texWidth * m_channels;
+	}
+
+	GLenum m_texType;
+	GLint m_format, m_texWidth, m_texHeight;
+	uint32_t m_byteCnt;
+	uint8_t m_channels;
+	unsigned char* m_texData;
+
+};
